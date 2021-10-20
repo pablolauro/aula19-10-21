042c065a2cee309e881c12bc17043b50b10fdc0b
pablolauro <pablo.lauro@gmail.com>
2021-10-20 18:58:54 -0300

    projeto inicial

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a primeira nota: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a segunda nota: ");
+            int n2 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Informe a terceira nota: ");
+            int n3 = int.Parse(Console.ReadLine());
+
+            double media = (n1 + n2 + n3) / 3;
+
+            Console.WriteLine("Média {0}  ",media);
+
+
+            Console.ReadKey();
+        }
+    }
+}
